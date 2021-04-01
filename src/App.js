926d0ac6e9b8923401c72402b772448c01f41818
tika27 <77032551+tika27@users.noreply.github.com>
2021-03-31 18:30:59 -0600

    crashed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <div className="app">
+      App
     </div>
   );
 }
